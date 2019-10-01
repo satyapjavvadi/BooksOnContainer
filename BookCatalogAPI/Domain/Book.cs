66ddc1f165514b5a37741bfa9a1ab10934f3ac84
Satya P Javvadi <saphin.rao@gmail.com>
2019-09-30 23:39:06 -0700

    Added Domain and Data folders

---
+++ BookCatalogAPI/Domain/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCatalogAPI.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public string ISBN { get; set; }
+        public string PublishedDate { get; set; }
+        public int CategoryId { get; set; }
+        public virtual BookCategory BookCategory { get; set; }
+        public int AuthorId { get; set; }
+        public virtual BookAuthor BookAuthor { get; set; }
+    }
+}
---
+++ BookCatalogAPI/Domain/BookStock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCatalogAPI.Domain
+{
+    public class BookStock
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
